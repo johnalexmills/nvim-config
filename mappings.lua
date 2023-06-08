65b9b826c5e4c2e9ee813934cf77c75492358311
Alex Mills <alexmills@Alexs-MacBook-Air.local>
2023-06-07 23:13:59 -0400

    Added centered navigation

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,10 @@ M.general = {
   v = {
     ["J"] = {":m '>+1<CR>gv=gv", "Move Selection Down"},
     ["K"] = {":m '>-2<CR>gv=gv", "Move Selection Up"}
+  },
+  n = {
+    ["C-d"] = {"<C-d>zz", "Move Down Centered"},
+    ["C-u"] = {"<C-u>zz", "Move Up Centered"},
   }
 }
 
